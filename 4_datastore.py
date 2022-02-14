49cfaf40ea61daa6898b69552faf7473c008a49a
Ty Liu <Ty_Liu1@baylor.edu>
2022-02-14 14:47:37 -0600

    note 2/14 not finished

--- 4_datastore.py
+++ 4_datastore.py
@@ -47,3 +47,12 @@ datastore = { "medical":[
 
       ]
 }
+outfile = open("retail_space.csv", "w")
+outfile.write("room-number, use, sq-ft, price\n")
+
+#print(datastore["medical"])
+
+for l in datastore["medical"]:
+  outfile.write(str(l["room-numer"])+ ',' + str(l["use"])+ ','+ str(l["sq-ft"]) + ',' + str(l["price"]) + "\n")
+
+outfile.close()
